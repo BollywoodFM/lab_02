ec2c89e7cdf6ed20cd08eeba23a83773ec2e6dff
BollywoodFM <161155136+BollywoodFM@users.noreply.github.com>
2024-06-02 16:39:10 +0300

    Created conflict

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -4,6 +4,6 @@
 int main() {
     std::string name;
     std::cin >> name;
-    std::cout << "Hello world from " << name << std::endl;
+    std::cout << "Hello world from " << name << std::endl; //Comment to fuel conflict
     return 0;
 }
